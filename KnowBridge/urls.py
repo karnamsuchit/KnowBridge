6263dc8288875bbd9654da0e71d8869c3538e3fc
karnamsuchit <83052042+karnamsuchit@users.noreply.github.com>
2024-10-17 20:33:04 +0530

    Delete KnowBridge directory

--- KnowBridge/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import upload_syllabus
-
-urlpatterns = [
-    path('upload_syllabus/', upload_syllabus, name='upload_syllabus'),
-    # Add more paths for different views
-]
